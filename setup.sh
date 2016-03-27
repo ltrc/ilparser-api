4434d03a38f28491d822380f9659ff37c4d0d7ed
Nehal J Wani <nehaljw.kkd1@gmail.com>
2016-03-27 16:05:39 +0530

    No harm in passing env variables ;-)

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ set -e
 git submodule update --recursive --init
 git submodule foreach --recursive git pull origin master
 
-sudo pip install -U ./lib/common/indic-wx-converter
-sudo pip install -U ./lib/common/indic-tokenizer
-sudo pip install -U ./lib/common/ilparser
+sudo -E pip install -U ./lib/common/indic-wx-converter
+sudo -E pip install -U ./lib/common/indic-tokenizer
+sudo -E pip install -U ./lib/common/ilparser
 
 make -C ./lib/hin/morph/analyser
