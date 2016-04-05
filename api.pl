dde1c6746e7a40f2ea5281b358458f3d74ffd059
Nehal J Wani <nehaljw.kkd1@gmail.com>
2016-04-05 21:21:28 +0530

    Add route /parse/:lang

--- api.pl
+++ api.pl
@@ -23,6 +23,20 @@ any '/parse' => sub {
     }
 };
 
+any '/parse/:lang' => sub {
+    my $c = shift;
+    my $lang = $c->param('lang');
+    my %args = %{$c->req->params->to_hash};
+    my $parser = get_parser($lang);
+    my $final_result = $parser->parse(%args);
+    if (exists $args{"pretty"}) {
+        my $final_string = join "\n", map { "$_:\n$final_result->{$_}" } keys %$final_result;
+        $c->render(template => 'pretty', result => $final_string);
+    } else {
+        $c->render(json => $final_result);
+    }
+};
+
 initialize_langs();
 
 app->start;
